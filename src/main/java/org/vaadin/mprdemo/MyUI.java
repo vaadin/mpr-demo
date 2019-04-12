594d2a2da19f1a56584a0e503a81ca1fe7327c99
tatulund <tatu@vaadin.com>
2019-04-12 13:22:56 +0300

    Migration step 1: Take MPR into use

--- src/main/java/org/vaadin/mprdemo/MyUI.java
+++ src/main/java/org/vaadin/mprdemo/MyUI.java
@@ -1,32 +1,22 @@
 package org.vaadin.mprdemo;
 
-import javax.servlet.annotation.WebServlet;
-
-import com.vaadin.annotations.Theme;
-import com.vaadin.annotations.VaadinServletConfiguration;
-import com.vaadin.server.VaadinRequest;
-import com.vaadin.server.VaadinServlet;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Label;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.mpr.core.HasLegacyComponents;
+import com.vaadin.mpr.core.MprTheme;
 import com.vaadin.ui.TabSheet;
-import com.vaadin.ui.TextField;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
 
-@Theme("mytheme")
-public class MyUI extends UI {
+@Route("")
+@MprTheme("mytheme")
+public class MyUI extends FlexLayout implements HasLegacyComponents {
 
-    @Override
-    protected void init(VaadinRequest vaadinRequest) {
+    public MyUI () {
+    	setSizeFull();
         final TabSheet layout = new TabSheet();
         layout.addTab(new SpreadsheetView(),"Spreadsheet");
         layout.addTab(new VideoView(),"Video");
         
-        setContent(layout);
+        add(layout);
     }
 
-    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
-    public static class MyUIServlet extends VaadinServlet {
-    }
 }
