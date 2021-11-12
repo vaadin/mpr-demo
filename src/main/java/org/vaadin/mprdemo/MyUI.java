dfe3ba7342e4b18567f9dec30d4fb40ffb934e52
tatulund <tatu@vaadin.com>
2021-11-12 16:04:15 +0200

    Add workaround to disable debug window opening in production mode

--- src/main/java/org/vaadin/mprdemo/MyUI.java
+++ src/main/java/org/vaadin/mprdemo/MyUI.java
@@ -1,6 +1,7 @@
 package org.vaadin.mprdemo;
 
 import com.vaadin.annotations.Push;
+import com.vaadin.flow.component.AttachEvent;
 import com.vaadin.flow.component.HasElement;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
@@ -9,14 +10,16 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouterLayout;
 import com.vaadin.flow.router.RouterLink;
+
 import com.vaadin.mpr.core.LegacyUI;
 import com.vaadin.mpr.core.MprTheme;
-import com.vaadin.ui.Notification;
+import com.vaadin.server.VaadinSession;
 
 @Push
 @Route("")
@@ -30,41 +33,51 @@ public class MyUI extends AppLayout implements RouterLayout {
 	private FlexLayout childWrapper = new FlexLayout();
 	private String css;
 
-    @SuppressWarnings("deprecation")
-	public MyUI () {
-        DrawerToggle toggle = new DrawerToggle();
-        addToNavbar(toggle);
-        VerticalLayout layout = new VerticalLayout();
-        layout.add(new RouterLink(SpreadsheetView.TITLE, SpreadsheetView.class));
-        layout.add(new RouterLink(TreeView.TITLE, TreeView.class));
-        layout.add(new RouterLink(VideoView.TITLE, VideoView.class));
-        layout.add(new RouterLink(LegacyRoute.TITLE, LegacyRoute.class));
-        Icon button = VaadinIcon.USER.create();
-        button.addClickListener(event -> {
-        	getUI().ifPresent(ui -> {
-        		ui.getSession().getSession().invalidate();        		
-        		ui.getPage().setLocation("http://localhost:8080/");
-        	});
-        });
-        layout.add(button);
-        addToDrawer(layout);
+	@SuppressWarnings("deprecation")
+	public MyUI() {
+		DrawerToggle toggle = new DrawerToggle();
+		addToNavbar(toggle);
+		VerticalLayout layout = new VerticalLayout();
+		layout.add(new RouterLink(SpreadsheetView.TITLE, SpreadsheetView.class));
+		layout.add(new RouterLink(TreeView.TITLE, TreeView.class));
+		layout.add(new RouterLink(VideoView.TITLE, VideoView.class));
+		layout.add(new RouterLink(LegacyRoute.TITLE, LegacyRoute.class));
+		Icon button = VaadinIcon.USER.create();
+		button.addClickListener(event -> {
+			getUI().ifPresent(ui -> {
+				ui.getSession().getSession().invalidate();
+				ui.getPage().setLocation("http://localhost:8080/");
+			});
+		});
+		layout.add(button);
+		addToDrawer(layout);
+		if (VaadinSession.getCurrent().getService().getDeploymentConfiguration().isProductionMode()) {
+			Notification.show("Vaadin 8 runs in prod mode");
+		}
 
-        childWrapper.setSizeFull();
-        setContent(childWrapper);
-        		
+		childWrapper.setSizeFull();
+		setContent(childWrapper);
+
 		OldUI ui = (OldUI) OldUI.getCurrent();
 		Notification.show(ui.getHello());
 		ui.getPage().addUriFragmentChangedListener(event -> {
 			String uriFragment = event.getUriFragment().substring(1);
-			System.out.println("URI:"+uriFragment);
+			System.out.println("URI:" + uriFragment);
 			UI.getCurrent().navigate(uriFragment);
 		});
-    }
+	}
+
+	@Override
+	public void onAttach(AttachEvent event) {
+		if (event.getSession().getService().getDeploymentConfiguration().isProductionMode()) {
+			event.getUI().getPage().executeJs("window.vaadin.debug=false;");
+		}
+	}
 
 	@Override
 	public void showRouterLayoutContent(HasElement content) {
 		childWrapper.removeAll();
-		childWrapper.getElement().appendChild(content.getElement());		
+		childWrapper.getElement().appendChild(content.getElement());
 	}
 
 }
