f2a05998eb46afb200446abf00e159d70b88a2e6
Manolo Carrasco <manolo@vaadin.com>
2025-12-18 22:51:09 +0100

    Update to Vaadin 25.0.0

--- src/main/java/org/vaadin/mprdemo/ApplicationConfig.java
+++ src/main/java/org/vaadin/mprdemo/ApplicationConfig.java
@@ -1,5 +1,7 @@
 package org.vaadin.mprdemo;
 
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.theme.lumo.Lumo;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.component.page.Push;
 import com.vaadin.mpr.core.LegacyUI;
@@ -8,5 +10,6 @@ import com.vaadin.mpr.core.MprTheme;
 @Push
 @MprTheme("mytheme")
 @LegacyUI(OldUI.class)
+@StyleSheet(Lumo.STYLESHEET)
 public class ApplicationConfig implements AppShellConfigurator {
 }
